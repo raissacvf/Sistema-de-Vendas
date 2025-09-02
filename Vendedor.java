a1048f797fee2af714fbbe44991e2432fc6cd8a7
Raíssa de Castro <RAISSACVF@OUTLOOK.COM>
2025-09-02 17:18:10 -0300

    Create Vendedor.java

---
+++ Vendedor.java
@@ -0,0 +1,28 @@
+package Aula5.Trabalho;
+
+/**
+ *
+ * @author raiss
+ */
+
+public class Vendedor {
+    private String nome;
+    private double comissaoTotal;
+
+    // O construtor agora só precisa do nome.
+    public Vendedor(String nome) {
+        this.setNome (nome);
+        this.comissaoTotal = 0.0;
+    }
+
+    public String getNome() { return this.nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    
+   
+    public double getComissaoTotal() { return this.comissaoTotal; }
+    public void setComissaoTotal(double comissaoTotal) { this.comissaoTotal = comissaoTotal; }
+    
+    public void adicionarComissao(double valorComissao) {
+        this.comissaoTotal += valorComissao;
+    }
+}
